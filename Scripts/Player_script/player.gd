1bddc3b8fab4a730ce0b806783c96ff5890c92a1
Berkay Sezan <brkysezan@gmail.com>
2025-07-06 19:06:46 +0300

    Cannon's are firing from ship but wrong direction

--- Scripts/Player_script/player.gd
+++ Scripts/Player_script/player.gd
@@ -1,17 +1,22 @@
 extends CharacterBody2D
 
+@onready var cannon_point_left = $CannonPointLeft
+@onready var cannon_point_right = $CannonPointRight
+
 var max_moving_speed = 100.0
 var rotation_speed = 1.0
 var direction := Vector2.ZERO
 var acceleration = 100.0
 var target_velocity = direction
+var cannonball_scene = preload("res://Scenes/reuseables_scene/cannonball.tscn")
 
 var input_state := {
 	"rotate_clockwise": false,
 	"rotate_c_clockwise": false,
 	"move_forward": false,
 	"move_backward": false,
-	"shoot": false,
+	"shoot_left": false,
+	"shoot_right": false,
 }
 
 func get_input_state() -> void:
@@ -19,7 +24,11 @@ func get_input_state() -> void:
 	input_state["rotate_clockwise"] = Input.is_action_pressed("rotate_clockwise")
 	input_state["move_forward"] = Input.is_action_pressed("move_forward")
 	input_state["move_backward"] = Input.is_action_pressed("move_backward")
-	input_state["shoot"] = Input.is_action_just_pressed("shoot")
+	input_state["shoot_left"] = Input.is_action_just_pressed("shoot_left")
+	input_state["shoot_right"] = Input.is_action_just_pressed("shoot_right")
+
+func _ready() -> void:
+	print("Cannonball direction: ", direction)
 
 func _physics_process(delta: float):
 	get_input_state()
@@ -48,5 +57,14 @@ func get_input(delta: float):
 		direction -= Vector2.RIGHT.rotated(rotation) * max_moving_speed / 5
 		
 	#shoot
-	if input_state["shoot"]:
-		pass
+	if input_state["shoot_left"]:
+		var cannonball = cannonball_scene.instantiate()
+		cannonball.position = cannon_point_left.global_position
+		cannonball.set_direction = (Vector2.RIGHT.rotated(rotation - PI / 2))
+		get_tree().current_scene.add_child(cannonball)
+
+	if input_state["shoot_right"]:
+		var cannonball = cannonball_scene.instantiate()
+		cannonball.position = cannon_point_right.global_position
+		cannonball.set_direction = (Vector2.RIGHT.rotated(rotation + PI / 2))
+		get_tree().current_scene.add_child(cannonball)
--- Scripts/reuseable_script/cannonball.gd
+++ Scripts/reuseable_script/cannonball.gd
@@ -1,13 +1,10 @@
 extends Area2D
 
-
-
 var speed = 300.0
 var direction = Vector2.ZERO
 
-func _ready():
-	direction = Vector2.RIGHT  # test movement to the right
-
+func set_direction(dir: Vector2):
+	direction = dir
 
 func _physics_process(delta):
 	position += direction * speed * delta
